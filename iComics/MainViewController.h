8d117a40789728c512355d726bc5d89a55a63da7
Lion User <User@Lions-Mac.local>
2012-02-04 11:14:43 +0000

    Initial commit

---
+++ iComics/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  iComics
+//
+//  Created by Lion User on 04/02/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
